c7c4ebfe202b1610dbe6968d6ca0abf48b574f17
Pablo Panero <pablo.panero@cern.ch>
2020-07-12 22:07:37 +0200

    wordcloud: change sizes and color palette

--- webpack/js/wordcloud.js
+++ webpack/js/wordcloud.js
@@ -4,41 +4,43 @@ var cloud = require("d3-cloud");
 window.addEventListener('load', function () {
   var myWords = [
     // Programming languages
-    {word: "Python", size: "100"},
-    {word: "JavaScript", size: "60"},
-    {word: "Bash", size: "60"},
-    {word: "Java", size: "50"},
-    //Frameworks
-    {word: "Invenio", size: "90"},
-    {word: "Flask", size: "80"},
-    {word: "React", size: "70"},
-    {word: "Tensorflow", size: "60"},
-    {word: "Keras", size: "60"},
+    {word: "Python", size: "100", cat: "plang"},
+    {word: "JavaScript", size: "50", cat: "plang"},
+    {word: "Bash", size: "30", cat: "plang"},
+    {word: "Java", size: "40", cat: "plang"},
+    // Frameworks
+    {word: "Invenio", size: "90", cat: "fw"},
+    {word: "Flask", size: "80", cat: "fw"},
+    {word: "React", size: "50", cat: "fw"},
+    {word: "Tensorflow", size: "60", cat: "fw"},
+    {word: "Keras", size: "40", cat: "fw"},
     // Databases
-    {word: "Elasticsearch", size: "90"},
-    {word: "PostgreSQL", size: "90"},
-    {word: "MySQL", size: "70"},
-    {word: "SQLite", size: "40"},
-    //DevOps
-    {word: "Docker", size: "90"},
-    {word: "OpenShift", size: "90"},
-    {word: "Helm", size: "60"},
-    {word: "Puppet", size: "50"},
-    {word: "Git", size: "80"},
-    {word: "Gitlab-CI", size: "60"},
-    {word: "GitHub Actions", size: "70"},
-    {word: "Travis CI", size: "90"},
-    //Big Data
-    {word: "Spark", size: "50"},
-    {word: "Kafka", size: "40"},
-    {word: "Flume", size: "40"},
-    {word: "YARN", size: "40"},
+    {word: "Elasticsearch", size: "90", cat: "db"},
+    {word: "PostgreSQL", size: "70", cat: "db"},
+    {word: "MySQL", size: "40", cat: "db"},
+    {word: "SQLite", size: "40", cat: "db"},
+    // DevOps
+    {word: "Docker", size: "90", cat: "devops"},
+    {word: "OpenShift", size: "90", cat: "devops"},
+    {word: "Helm", size: "60", cat: "devops"},
+    {word: "Puppet", size: "30", cat: "devops"},
+    {word: "Git", size: "80", cat: "devops"},
+    {word: "Gitlab-CI", size: "30", cat: "devops"},
+    {word: "GitHub Actions", size: "50", cat: "devops"},
+    {word: "Travis CI", size: "80", cat: "devops"},
+    // Data
+    {word: "Neural Networks", size: "70", cat: "data"},
+    {word: "Data visualization", size: "60", cat: "data"},
+    {word: "Spark", size: "40", cat: "data"},
+    {word: "Kafka", size: "30", cat: "data"},
+    {word: "Flume", size: "30", cat: "data"},
+    {word: "YARN", size: "30", cat: "data"},
   ]
 
   // set the dimensions and margins of the graph
-  var margin = {top: 10, right: 10, bottom: 10, left: 10},
+  var margin = {top: 5, right: 5, bottom: 5, left: 5},
       width = 1200 - margin.left - margin.right,
-      height = 600 - margin.top - margin.bottom;
+      height = 700 - margin.top - margin.bottom;
 
   // append the svg object to the body of the page
   var svg = d3.select("#skills-cloud").append("svg")
@@ -52,12 +54,28 @@ window.addEventListener('load', function () {
   // Wordcloud features that are different from one word to the other must be here
   var layout = cloud()
     .size([width, height])
-    .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
+    .words(myWords.map(function(d) { return {text: d.word, props: [d.size, d.cat]}; }))
     .padding(5) //space between words
-    .fontSize(function(d) { return d.size; })      // font size of words
+    .fontSize(function(d) { return d.props[0]; })      // font size of words
     .on("end", draw);
   layout.start();
 
+  function getColorFromCategory(category) {
+    switch(category) {
+      case "plang":
+        return "#E4E423";
+      case "fw":
+        return "#07AE39";
+      case "db":
+        return "#FD6933";
+      case "devops":
+        return "#2A15E2";
+      case "data":
+        return "#40EAF6";
+      default:
+        return "#FFFFFF";
+    }
+  }
   // This function takes the output of 'layout' above and draw the words
   // Wordcloud features that are THE SAME from one word to the other can be here
   function draw(words) {
@@ -67,8 +85,8 @@ window.addEventListener('load', function () {
         .selectAll("text")
           .data(words)
         .enter().append("text")
-          .style("font-size", function(d) { return d.size; })
-          .style("fill", "#000000")
+          .style("font-size", function(d) { return d.props[0]; })
+          .style("fill", function (d) { return getColorFromCategory(d.props[1]);})
           .attr("text-anchor", "middle")
           .style("font-family", "Impact")
           .attr("transform", function(d) {
