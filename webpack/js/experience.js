2fbc6def63820739f70c81535e0ed178b87cc0a0
Pablo Panero <pablo.panero@cern.ch>
2020-07-18 23:58:41 +0200

    experience: migrate jobs to html and fix read more btn selector

--- webpack/js/experience.js
+++ webpack/js/experience.js
@@ -3,11 +3,11 @@ window.addEventListener('load', function () {
         $('#exp-list .d-none').slice(0, 2).removeClass("d-none");
     });
 
-    $('#show-more-job').click(function () {
+    $('.show-more-job').click(function () {
         var content = $(this).parent().parent();
         var dots = content.find("#dots")[0];
         var moreText = content.find("#more-text")[0];
-        var moreBtn = content.find("#show-more-job")[0];
+        var moreBtn = content.find(".show-more-job")[0];
       
         if (dots.style.display === "none") {
           dots.style.display = "inline";
