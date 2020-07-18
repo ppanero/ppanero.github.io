5eada06f704d583b0d1ae8639f7cdc22713ce4be
Pablo Panero <pablo.panero@cern.ch>
2020-07-18 23:38:01 +0200

    experience: make job description html and improve read more style

--- webpack/js/experience.js
+++ webpack/js/experience.js
@@ -1,21 +1,22 @@
 window.addEventListener('load', function () {
-    $(".show-more-job p").click(function() {
-        var $this = $(this); 
-        var $content = $this.parent().prev("div.content");
-        var linkText = $this.text();    
-        
-        if(linkText === "[more]"){
-            linkText = "[less]";
-            $content.removeClass("hide-content").addClass("show-content")
-        } else {
-            linkText = "[more]";
-            $content.removeClass("show-content").addClass("hide-content")
-        };
-
-        $this.text(linkText);
-    });
-
     $('.show-more-exp').click(function () {
         $('#exp-list .d-none').slice(0, 2).removeClass("d-none");
+    });
+
+    $('#show-more-job').click(function () {
+        var content = $(this).parent().parent();
+        var dots = content.find("#dots")[0];
+        var moreText = content.find("#more-text")[0];
+        var moreBtn = content.find("#show-more-job")[0];
+      
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          moreBtn.innerHTML = "Read more"; 
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          moreBtn.innerHTML = "Read less"; 
+          moreText.style.display = "inline";
+        }
     });
 })
